21eac035c4d257559bd9576cde5d477102ccb485
carolynlu <carolynlu06@gmail.com>
2024-09-21 01:19:25 -0500

    code from vid

---
+++ settings.py
@@ -0,0 +1,13 @@
+# Game Setup
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+
+# Player Settings
+PLAYER_START_X = 400
+PLAYER_START_Y = 500
+PLAYER_SIZE = 0.2
+PLAYER_SPEED = 8
+
+# Bullet Settings
+SHOOT_COOLDOWN = 20
