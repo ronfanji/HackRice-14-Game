900a974c95db23d8e6f0328a88d8e58028ab2ef2
carolynlu <carolynlu06@gmail.com>
2024-09-21 12:36:12 -0500

    enemy class

--- game1.py
+++ game1.py
@@ -9,8 +9,9 @@ HEIGHT = 800
 FPS = 60
 PLAYER_START_X = WIDTH//2
 PLAYER_START_Y = HEIGHT//2
-PLAYER_SIZE = 0.05
+PLAYER_SIZE = 0.125
 PLAYER_SPEED = 8
+ENEMY_SPEED = 4
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RADish Beets 'em Up")
@@ -21,7 +22,10 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
-        self.image = pygame.transform.rotozoom(pygame.image.load("hackathon/radish.png").convert_alpha(), 0, PLAYER_SIZE) # change later
+        self.image = pygame.transform.rotozoom(pygame.image.load("hackathon/radish_not_png.jpg").convert_alpha(), 0, PLAYER_SIZE) # change later
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
 
     def user_input(self):
@@ -53,11 +57,51 @@ class Player(pygame.sprite.Sprite):
         elif self.pos.y > HEIGHT - self.image.get_height():
             self.pos.y = HEIGHT - self.image.get_height()
 
+        self.rect.center = self.pos
+
     def update(self):
         self.user_input()
         self.move()
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, position):
+        super().__init__(enemy_group, all_sprites_group)
+        self.image = pygame.image.load("hackathon/enemy_not_png.jpg").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, 0.125)
+
+        self.rect = self.image.get_rect(center = position)
+        self.speed = ENEMY_SPEED
+
+        #self.position = pygame.math.Vector2(position)
+
+    def follow_player(self):
+        player_vector = pygame.math.Vector2(player.pos)
+        enemy_vector = pygame.math.Vector2(self.rect.center)
+        distance = self.distance(player_vector, enemy_vector)
+
+        if distance > 0:
+            self.direction = (player_vector - enemy_vector).normalize()
+            self.rect.center += self.direction * self.speed
+ 
+        else:
+            self.direction = pygame.math.Vector2()
+
+    def distance(self, vector1, vector2):
+        return (vector1 - vector2).magnitude()
+    
+    def update(self):
+        self.follow_player()
+
+all_sprites_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+
 player = Player()
+enemy = Enemy((300, 300))
+
+all_sprites_group.add(player)
+all_sprites_group.add(enemy)
+
+game_over = False
 
 while True:
     keys = pygame.key.get_pressed()
@@ -65,10 +109,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    if not game_over:
+        screen.blit(background, (0, 0))
+
+        all_sprites_group.update()
+        all_sprites_group.draw(screen)
+        
+        if pygame.sprite.collide_rect(player,enemy):
+            game_over = True
+
     
-    screen.blit(background, (0,0))
-    screen.blit(player.image, player.pos)
-    player.update()
+    #screen.blit(player.image, player.pos)
+    #player.update()
 
     pygame.display.update()
     clock.tick(FPS)
